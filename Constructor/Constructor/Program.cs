320defc1e8c47f8c887187f010ab41d07ca66b81
Divyansh174 <divyansh.pal@coditas.com>
2022-11-16 16:27:17 +0530

    added constructor

---
+++ Constructor/Constructor/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Constructor;
+
+Warrior warrior = new Warrior(3,60);
+Console.WriteLine($"The height of the Warrior is {warrior.height} metres and the weight is {warrior.weight} kg");
---
+++ Constructor/Constructor/Warrior.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    public class Warrior
+    {
+        public int height { get; set; }
+
+        public int weight { get; set; }
+
+        public Warrior(int height, int weight) 
+        {
+            this.height = height;
+            this.weight = weight;
+        }
+    }
+}
